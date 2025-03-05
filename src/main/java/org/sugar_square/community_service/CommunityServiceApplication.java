5179d1cd16dada38d2ffefec1259bfb2134de33d
hyeonwoo <ziczin7176@gmail.com>
2025-03-05 20:34:37 +0900

    Project init
    프로젝트 생성

---
+++ src/main/java/org/sugar_square/community_service/CommunityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sugar_square.community_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sugar_square/community_service/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sugar_square.community_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
