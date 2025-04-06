a54aec79a0e8253100c3c1b3d05680d4b70b7a6d
hyeonwoo <ziczin7176@gmail.com>
2025-04-06 19:21:17 +0900

    Feat : Post > 단일 조회 구현

--- src/main/java/org/sugar_square/community_service/Controller/board/PostController.java
+++ src/main/java/org/sugar_square/community_service/Controller/board/PostController.java
@@ -4,19 +4,24 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.sugar_square.community_service.dto.board.PostRequestDTO;
+import org.sugar_square.community_service.dto.board.PostResponseDTO;
 import org.sugar_square.community_service.service.board.PostService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class PostController {
 
   private final PostService postService;
 
-  @PostMapping("/board")
+  @PostMapping
   public ResponseEntity<String> writePost(@RequestBody @Valid final PostRequestDTO requestDTO) {
     Long savedPostId = postService.register(
         requestDTO.title(),
@@ -24,6 +29,13 @@ public class PostController {
         requestDTO.memberId(),
         requestDTO.categoryId()
     );
-    return ResponseEntity.status(HttpStatus.CREATED).body("postId : " + savedPostId);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body("postId : " + savedPostId); // TODO: 페이지 리다이렉트
+  }
+
+  @GetMapping("/{postId}")
+  public ResponseEntity<PostResponseDTO> readPost(@PathVariable Long postId) {
+    PostResponseDTO responseDto = postService.readOneById(postId);
+    return ResponseEntity.status(HttpStatus.OK).body(responseDto);
   }
 }
