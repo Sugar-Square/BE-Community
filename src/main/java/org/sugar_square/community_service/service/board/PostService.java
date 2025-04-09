dd31c7f3dc4aa5d0833c4c071629e61e8ae41cab
hyeonwoo <ziczin7176@gmail.com>
2025-04-10 02:25:51 +0900

    Refactor : Post 등록 > 함수 시그니처 변경

--- src/main/java/org/sugar_square/community_service/service/board/PostService.java
+++ src/main/java/org/sugar_square/community_service/service/board/PostService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.sugar_square.community_service.domain.board.Category;
 import org.sugar_square.community_service.domain.board.Post;
 import org.sugar_square.community_service.domain.member.Member;
+import org.sugar_square.community_service.dto.board.PostRegisterDTO;
 import org.sugar_square.community_service.dto.board.PostResponseDTO;
 import org.sugar_square.community_service.exception.EntityNotFoundException;
 import org.sugar_square.community_service.repository.board.PostRepository;
@@ -23,14 +24,12 @@ public class PostService {
   private final MemberService memberService;
 
   @Transactional(readOnly = false)
-  public Long register(
-      final String title, final String content, final Long memberId, final Long categoryId
-  ) {
-    Member writer = memberService.findOneById(memberId);
-    Category category = categoryService.findOneById(categoryId);
+  public Long register(PostRegisterDTO registerDTO) {
+    Member writer = memberService.findOneById(registerDTO.memberId());
+    Category category = categoryService.findOneById(registerDTO.categoryId());
     Post registered = Post.builder()
-        .title(title)
-        .content(content)
+        .title(registerDTO.title())
+        .content(registerDTO.content())
         .writer(writer)
         .category(category)
         .build();
