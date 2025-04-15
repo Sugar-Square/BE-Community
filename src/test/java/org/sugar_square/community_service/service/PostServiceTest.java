54d56cc55ed550577553437ad0f278fea07ef453
hyeonwoo <ziczin7176@gmail.com>
2025-04-16 03:37:08 +0900

    Test : PostService > soft delete 테스트

--- src/test/java/org/sugar_square/community_service/service/PostServiceTest.java
+++ src/test/java/org/sugar_square/community_service/service/PostServiceTest.java
@@ -5,6 +5,7 @@ import static org.sugar_square.community_service.TestDataInitializer.MEMBER_NICK
 import static org.sugar_square.community_service.TestDataInitializer.POST_TITLE;
 
 import java.util.List;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -31,13 +32,14 @@ import org.sugar_square.community_service.service.board.PostService;
 public class PostServiceTest {
 
   @Autowired
-  MemberRepository memberRepository;
+  private MemberRepository memberRepository;
 
   @Autowired
-  CategoryRepository categoryRepository;
+  private CategoryRepository categoryRepository;
 
   @Autowired
-  PostService postService;
+  private PostService postService;
+
   @Autowired
   private PostRepository postRepository;
 
@@ -54,27 +56,46 @@ public class PostServiceTest {
   @Test
   @DisplayName("게시물 저장 테스트")
   void registerTest() {
+    //given
     List<Member> members = memberRepository.findByNickname(MEMBER_NICKNAME + "0");
     List<Category> categories = categoryRepository.findByName(CATEGORY_NAME + "0");
     PostRegisterDTO registerDTO = new PostRegisterDTO("test title", "test content",
         members.getFirst().getId(),
         categories.getFirst().getId());
+    //when
     Long registeredId = postService.register(registerDTO);
+    //then
     Assertions.assertThat(registeredId).isNotNull();
   }
 
   @Test
   @DisplayName("게시물 수정 테스트")
   void modifyTest() {
+    //given
     List<Category> categories = categoryRepository.findByName(CATEGORY_NAME + "0");
     List<Post> posts = postRepository.findByTitle(POST_TITLE + "0");
     Long categoryId = categories.getFirst().getId();
     Long postId = posts.getFirst().getId();
     PostModifyDTO modifyDTO = new PostModifyDTO("modified title", "modified content", categoryId);
+    //when
     postService.modify(postId, modifyDTO);
+    //then
     Post modifiedPost = postService.findOneById(postId);
     Assertions.assertThat(modifiedPost)
         .extracting("title", "content", "category.id") // 객체 속성 추출
         .containsExactly("modified title", "modified content", categoryId); // 모든 값, 순서 검증
+  }
+
+  @Test
+  @DisplayName("게시물 삭제 테스트")
+  void softDeleteTest() {
+    //given
+    List<Post> posts = postRepository.findByTitle(POST_TITLE + "0");
+    Long postId = posts.getFirst().getId();
+    //when
+    postService.softDelete(postId);
+    //then
+    Optional<Post> found = postRepository.findById(postId);
+    Assertions.assertThat(found).isEmpty();
   }
 }
