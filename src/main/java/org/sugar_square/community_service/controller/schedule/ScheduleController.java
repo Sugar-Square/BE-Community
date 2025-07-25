a424dac092b06dd693b709c09aa962a5195601f6
hyeonwoo <ziczin7176@gmail.com>
2025-07-25 21:52:54 +0900

    feat : 월별 일정 목록 조회 구현

--- src/main/java/org/sugar_square/community_service/controller/schedule/ScheduleController.java
+++ src/main/java/org/sugar_square/community_service/controller/schedule/ScheduleController.java
@@ -1,19 +1,24 @@
 package org.sugar_square.community_service.controller.schedule;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.sugar_square.community_service.domain.schedule.Schedule;
 import org.sugar_square.community_service.dto.schedule.ScheduleModifyDTO;
 import org.sugar_square.community_service.dto.schedule.ScheduleRegisterDTO;
+import org.sugar_square.community_service.dto.schedule.ScheduleResponseDTO;
 import org.sugar_square.community_service.service.schedule.ScheduleService;
 
 @RestController
@@ -22,6 +27,15 @@ import org.sugar_square.community_service.service.schedule.ScheduleService;
 public class ScheduleController {
 
   private final ScheduleService scheduleService;
+
+  // 월 단위 일정 조회 (ex: 2025-01, 2025-02 ...)
+  @GetMapping
+  public ResponseEntity<List<ScheduleResponseDTO>> readSchedulesForMonth(
+      @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM") final String yearMonth
+  ) {
+    List<ScheduleResponseDTO> schedules = scheduleService.readForMonth(yearMonth);
+    return ResponseEntity.ok(schedules);
+  }
 
   @PostMapping
   public ResponseEntity<String> registerSchedule(
--- src/main/java/org/sugar_square/community_service/utils/StringDateConverter.java
+++ src/main/java/org/sugar_square/community_service/utils/StringDateConverter.java
@@ -3,6 +3,9 @@ package org.sugar_square.community_service.utils;
 import jakarta.annotation.Nullable;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import org.springframework.util.StringUtils;
 
@@ -22,5 +25,23 @@ public class StringDateConverter {
       return null;
     }
     return LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
+  }
+
+  public static InstantYearMonth stringYearMonthToInstantStartEnd(String yearMonth) {
+    YearMonth ym;
+    if (!StringUtils.hasText(yearMonth)) {
+      ym = YearMonth.now();
+    } else {
+      ym = YearMonth.parse(yearMonth);
+    }
+    LocalDateTime startOfMonth = ym.atDay(1).atStartOfDay();
+    LocalDateTime endOfMonth = ym.atEndOfMonth().atTime(23, 59, 59);
+    Instant startInstant = startOfMonth.toInstant(ZoneOffset.UTC);
+    Instant endInstant = endOfMonth.toInstant(ZoneOffset.UTC);
+    return new InstantYearMonth(startInstant, endInstant);
+  }
+
+  public record InstantYearMonth(Instant start, Instant end) {
+
   }
 }
