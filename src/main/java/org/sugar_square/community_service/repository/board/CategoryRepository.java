5053b747ab2885759e5b96d67269160beb51b819
hyeonwoo <ziczin7176@gmail.com>
2025-08-22 22:10:47 +0900

    feat : category 이름 중복체크 > 공백제거, 소문자 비교 처리

--- src/main/java/org/sugar_square/community_service/repository/board/CategoryRepository.java
+++ src/main/java/org/sugar_square/community_service/repository/board/CategoryRepository.java
@@ -1,5 +1,7 @@
 package org.sugar_square.community_service.repository.board;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.sugar_square.community_service.domain.board.Category;
 import org.sugar_square.community_service.repository.BaseRepository;
@@ -7,5 +9,7 @@ import org.sugar_square.community_service.repository.BaseRepository;
 @Repository
 public interface CategoryRepository extends BaseRepository<Category, Long> {
 
-  boolean existsByName(String name);
+  // 공백에 제거된 카테고리 이름끼리 비교하여 중복된 이름이 있는지 확인. for PostgreSQL
+  @Query("SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM Category c WHERE REPLACE(c.name, ' ', '') = :name")
+  boolean existsByNameWithoutSpaces(@Param("name") String name);
 }
